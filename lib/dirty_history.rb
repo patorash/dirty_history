584ecb2484009a3c564c9882f536befeb01caffc
Gavin Todes <gavin.todes@gmail.com>
2011-04-30 18:02:58 -0500

    added DirtyHistory module

--- lib/dirty_history.rb
+++ lib/dirty_history.rb
@@ -2,3 +2,9 @@ require "dirty_history/dirty_history"
 require "dirty_history/dirty_history_mixin" 
 
 ActiveRecord::Base.send(:include, DirtyHistoryMixin)
+
+module DirtyHistory
+  def self.setup
+    yield self
+  end
+end
