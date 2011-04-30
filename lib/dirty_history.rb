e69866fc263fdf466a0fece7b6c147122cbc151b
Gavin Todes <gavin.todes@gmail.com>
2011-04-30 18:06:43 -0500

    moved DirtyHistory module declaration to the right spot

--- lib/dirty_history.rb
+++ lib/dirty_history.rb
@@ -1,10 +1,11 @@
+require 'rails'
 require "dirty_history/dirty_history" 
 require "dirty_history/dirty_history_mixin" 
-
-ActiveRecord::Base.send(:include, DirtyHistoryMixin)
 
 module DirtyHistory
   def self.setup
     yield self
   end
 end
+
+ActiveRecord::Base.send(:include, DirtyHistoryMixin)
