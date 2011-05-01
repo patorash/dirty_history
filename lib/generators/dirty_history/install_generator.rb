fc2f1a6c3eb882f10d480295196d2ae16e3632e1
Gavin Todes <gavin.todes@gmail.com>
2011-05-01 14:12:59 -0500

    stuff

--- lib/generators/dirty_history/install_generator.rb
+++ lib/generators/dirty_history/install_generator.rb
@@ -1,6 +1,6 @@
-require 'rails/generators'
+require 'rails/generators/migration'
 
-module DH
+module DirtyHistory
   class InstallGenerator < Rails::Generators::Base          
     
     source_root File.expand_path("../templates", __FILE__)
