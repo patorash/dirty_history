99fed246ceef91446ceb6d9750269e1782227775
Gavin Todes <gavin.todes@gmail.com>
2011-05-03 00:24:17 -0500

    remove validation of old_value because newly created object attributes will not have an old value

--- lib/dirty_history/dirty_history_record.rb
+++ lib/dirty_history/dirty_history_record.rb
@@ -1,7 +1,8 @@
 class DirtyHistoryRecord < ActiveRecord::Base   
   belongs_to :creator,  :polymorphic => true
   belongs_to :object,   :polymorphic => true
-  validates_presence_of :object_type, :object_id, :column_name, :column_type, :old_value, :new_value
+  validates_presence_of :object_type, :object_id, :column_name, :column_type, :new_value
+  # can't validate present of old_value because new object attributes won't have an old value
   
   scope :created_by,      lambda { |creator| where(:creator_id => creator.id, :creator_type => creator.class.name) }
   scope :for_object_type, lambda { |object_type| where(:object_type => object_type.to_s.classify) }
