60db4d705c4b184658e6248e15f7e0d0fee69f6d
Gavin Todes <gavin.todes@gmail.com>
2012-08-24 12:42:42 -0500

    better indices and field types

--- lib/generators/dirty_history/migration/templates/active_record/migration.rb
+++ lib/generators/dirty_history/migration/templates/active_record/migration.rb
@@ -1,12 +1,14 @@
 class CreateDirtyHistoryRecords < ActiveRecord::Migration
   def self.up
     create_table :dirty_history_records do |t|                                           
-      t.references :creator,  :polymorphic => true # creates creator_type and creator_id field
-      t.references :object,   :polymorphic => true # creates object_type and object_id field            
-      t.string     :column_name, :length => 64
-      t.string     :column_type, :length => 16 # :string, :text, :integer, :decimal, :float, :datetime, :boolean
-      t.text       :old_value
-      t.text       :new_value
+      t.integer    :creator_id
+      t.string     :creator_type, :limit => 64
+      t.integer    :object_id
+      t.string     :object_type,  :limit => 64
+      t.string     :column_name,  :limit => 64
+      t.string     :column_type,  :limit => 16 # :string, :text, :integer, :decimal, :float, :datetime, :boolean
+      t.string     :old_value,    :limit => 128
+      t.string     :new_value,    :limit => 128
     
       t.datetime   :created_at
       t.datetime   :revised_created_at
@@ -15,6 +17,14 @@ class CreateDirtyHistoryRecords < ActiveRecord::Migration
     end
     add_index :dirty_history_records, [:creator_id, :creator_type]
     add_index :dirty_history_records, [:object_id,  :object_type]
+    add_index :dirty_history_records, [:created_at,  :revised_created_at], :name => "index_created_at"
+    add_index :dirty_history_records, [:deleted_at, :created_at,  :revised_created_at], :name => "index_deleted_at_created_at"
+    add_index :dirty_history_records, [:object_type, :column_name, :deleted_at], :name => "index_obj_type_column_deleted_at"
+    add_index :dirty_history_records, [:object_type, :column_name, :deleted_at, :created_at, :revised_created_at], :name => "index_obj_type_column_deleted_at_created_at"
+
+    add_index :dirty_history_records, :old_value, :name => "index_old_value"
+    add_index :dirty_history_records, :new_value, :name => "index_new_value"
+    add_index :dirty_history_records, [:old_value, :new_value], :name => "index_old_value_new_value"
   end
 
   def self.down
